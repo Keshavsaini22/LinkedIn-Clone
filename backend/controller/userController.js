7c2511e3663b9a0e865ba490645f3d3d49a49bf4
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-02-15 19:04:27 +0530

    updated user api

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,7 +2,7 @@ const { userService } = require("../services")
 
 exports.updateUserProfile = async (req, res) => {
     try {
-        const response = await userService.updateUserProfile(req);
+        const response = await userService.updateUserProfile({params:req.params,files:req.files,data:req.body});
         return res.status(200).json(response);
     }
     catch (e) {
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,21 +1,17 @@
 const UsersModel = require('../models/UserSchema')
 const CustomError = require('../libs/error');
 
-exports.updateUserProfile = async (req) => {
-    const { userId } = req.params;
-    if (userId) {
-        const image = req.files.image[0].path;
-        const { name, phone, website, languages, title, desc, industry } = req.body;
-
-        if (req.body.address) {
-            var { street, state, city, pincode, country } = JSON.parse(req.body.address)
-        }
-
-        const data = await UsersModel.findByIdAndUpdate(userId, {
-            name, address: { street, state, city, pincode, country },
-            phone, website, image, title, desc, industry, languages
-        }, { new: true });
-        return data;
+exports.updateUserProfile = async (payload) => {
+    const { userId } = payload.params;
+    const image = payload.files.image[0].path;
+    const { name, phone, website, languages, title, desc, industry } = payload.data;
+    if (req.body.address) {
+        var { street, state, city, pincode, country } = JSON.parse(payload.data.address)
     }
-    throw new CustomError("Bad request", 404);
+    const data = await UsersModel.findByIdAndUpdate(userId, {
+        name, address: { street, state, city, pincode, country },
+        phone, website, image, title, desc, industry, languages
+    }, { new: true });
+    return data;
 }
+  
