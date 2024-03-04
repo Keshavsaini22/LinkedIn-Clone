6c849e30a18e4710da390327d74ff8228273c25a
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-03-04 16:55:54 +0530

    dfdfdf

--- frontend/src/services/network.service.js
+++ frontend/src/services/network.service.js
@@ -1,3 +1,4 @@
 import axios from 'axios'
-const backendUrl =process.env.REACT_APP_BACKEND_URL
-export const getSuggestionsService=(config)=> axios.get(`${backendUrl}/connection/suggestions`, config)
+const backendUrl = process.env.REACT_APP_BACKEND_URL
+// export const getSuggestionsService=(config)=> axios.get(`${backendUrl}/connection/suggestions`, config)
+export const getSuggestionsService = (config) => axios.get(`http://localhost:8080/connection/suggestions`, config)
